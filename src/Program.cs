7cee9cbcb38fbccbc10f2510f85f85c0d49e0c76
Yury Belousov <yury-belousov@yandex.ru>
2018-03-14 03:38:07 +0300

    add geo converters, adj list & matrix, node info

--- src/Program.cs
+++ src/Program.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Xml.Serialization;
 using CityMap.Types;
 
@@ -9,39 +11,53 @@ namespace CityMap
 {
     internal static class Program
     {
-        private static string ConvertCoordinates(double coordinate)
+        private static string ConvertToGeo(GeoPoint currentPoint, GeoPoint minPoint)
         {
-            return (coordinate * 1000).ToString(System.Globalization.CultureInfo.InvariantCulture);
+            return $"{(currentPoint.X - minPoint.X) / 100.0} {(currentPoint.Y - minPoint.Y) / 100.0}";
+        }
+
+        private static string WriteAdjust(KeyValuePair<ulong, GeoPoint> point, IEnumerable<ulong> keys)
+        {
+            var sb = new StringBuilder();
+            foreach (var key in keys)
+            {
+                sb.Append(point.Value.Adjency.Contains(key) ? ", 1" : ", 0");
+            }
+            return sb.ToString();
         }
 
         private static void Main(string[] args)
         {
             City volgograd;
 
-            using (var reader = new StreamReader("volgograd.osm"))
+            using (var reader = new StreamReader("VGG.osm"))
             {
                 volgograd = (City)new XmlSerializer(typeof(City)).Deserialize(reader);
             }
 
-            var nodesGeo = new Dictionary<ulong, (double longitude, double latitude, bool used)>(volgograd.Nodes.Length);
+            var nodesGeo = new Dictionary<ulong, GeoPoint>(volgograd.Nodes.Length);
+            // TODO: if no bounds
+            var minimumPoint = new GeoPoint(volgograd.Bounds.MinimumLongitude, volgograd.Bounds.MinimumLatitude);
+            var maxPoint = new GeoPoint(volgograd.Bounds.MaximumLongitude, volgograd.Bounds.MaximumLatitude);
 
             var culture =
                 (System.Globalization.CultureInfo)System.Threading.Thread.CurrentThread.CurrentCulture.Clone();
             culture.NumberFormat.NumberDecimalSeparator = ".";
             System.Threading.Thread.CurrentThread.CurrentCulture = culture;
 
-            Directory.CreateDirectory("output");
-            
-            using (StreamWriter output = new StreamWriter(Path.Combine("output", "map.svg")),
-                nodesWrite = new StreamWriter(Path.Combine("output", "nodes.csv")),
-                adjacencyMatrix = new StreamWriter(Path.Combine("output", "adjacency_matrix.csv")),
-                adjacencyList = new StreamWriter(Path.Combine("output", "adjacency_list.csv")))
+            const string outputDirectory = "Output";
+            Directory.CreateDirectory(outputDirectory);
+
+            using (StreamWriter output = new StreamWriter(Path.Combine(outputDirectory, "map.svg")),
+                nodesWrite = new StreamWriter(Path.Combine(outputDirectory, "nodes.csv")),
+                adjacencyMatrix = new StreamWriter(Path.Combine(outputDirectory, "adjacency_matrix.csv")),
+                adjacencyList = new StreamWriter(Path.Combine(outputDirectory, "adjacency_list.csv")))
             {
                 output.WriteLine("<?xml version=\"1.0\" standalone=\"no\"?>\r\n<svg version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\">");
 
                 foreach (var node in volgograd.Nodes)
                 {
-                    nodesGeo.Add(node.Id, (node.Longitude, node.Latitude, false));
+                    nodesGeo.Add(node.Id, new GeoPoint(node.Longitude, node.Latitude));
                     //output.WriteLine($"<circle cx=\"{ConvertCoordinates(node.Longitude - volgograd.Bounds.MinimumLongitude)}\" cy=\"{ConvertCoordinates(node.Latitude - volgograd.Bounds.MinimumLatitude)}\" r=\"0.03\" fill=\"red\" />");
                 }
 
@@ -49,16 +65,28 @@ namespace CityMap
                 {
                     if (way.Tags?.FirstOrDefault(t => t.Key == "highway") != null)
                     {
+                        var index = way.Nodes.ToList().FindIndex(n => nodesGeo.ContainsKey(n.Reference));
+
+                        if (index == -1) continue;
+
+                        var previuosNode = way.Nodes[index].Reference;
+
                         output.Write("<polyline points=\"");
-                        IList<string> nodes = new List<string>(way.Nodes.Length);
-                        foreach (var node in way.Nodes)
+                        output.Write(ConvertToGeo(nodesGeo[previuosNode], minimumPoint));
+
+                        foreach (var node in way.Nodes.ToList().Skip(index + 1))
                         {
                             if (nodesGeo.ContainsKey(node.Reference))
                             {
-                                var (longitude, latitude, _) = nodesGeo[node.Reference];
-                                nodesGeo[node.Reference] = (longitude, latitude, true);
-                                nodes.Add(
-                                    $"{ConvertCoordinates(longitude - volgograd.Bounds.MinimumLongitude)} {ConvertCoordinates(latitude - volgograd.Bounds.MinimumLatitude)}");
+                                var point = nodesGeo[node.Reference];
+                                point.Used = true;
+
+                                point.Adjency.Add(previuosNode);
+                                nodesGeo[previuosNode].Adjency.Add(node.Reference);
+
+
+                                output.Write($", {ConvertToGeo(point, minimumPoint)}");
+                                previuosNode = node.Reference;
                             }
                             else
                             {
@@ -66,26 +94,38 @@ namespace CityMap
                             }
                         }
 
-                        output.Write(string.Join(", ", nodes));
-
-                        output.WriteLine("\" stroke=\"blue\" fill=\"transparent\" stroke-width=\"0.1\"/>");
+                        output.WriteLine(way.Tags?.FirstOrDefault(t => t.Value == "primary") != null
+                            ? "\" stroke=\"brown\" fill=\"transparent\" stroke-width=\"0.6\"/>"
+                            : "\" stroke=\"blue\" fill=\"transparent\" stroke-width=\"0.1\"/>");
                     }
                 }
-                nodesWrite.WriteLine("id,longitude,latitude");
-
-                foreach (var node in nodesGeo)
-                {
-                    if (node.Value.used)
-                    {
-                        //output.WriteLine($"<circle cx=\"{ConvertCoordinates(node.Value.longitude - volgograd.Bounds.MinimumLongitude)}\" cy=\"{ConvertCoordinates(node.Value.latitude - volgograd.Bounds.MinimumLatitude)}\" r=\"0.05\" fill=\"red\" />");
-                        nodesWrite.WriteLine($"{node.Key},{node.Value.longitude},{node.Value.latitude}");
-                    }
-                }
-
                 output.WriteLine("</svg>");
+
+                nodesGeo = nodesGeo.Where(n => n.Value.Used).ToDictionary(n => n.Key, n => n.Value);
+
+                nodesWrite.WriteLine("Id, Latitude, Longitude, X, Y");
+                adjacencyMatrix.WriteLine($", {string.Join(", ", nodesGeo.Keys)}");
+                adjacencyList.WriteLine("Node, Adjust");
+
+                var count = 0;
+                var edges = 0;
+                foreach (var node in nodesGeo.AsParallel())
+                {
+                    ++count;
+                    edges += node.Value.Adjency.Count;
+                    nodesWrite.WriteLine(
+                        $"{node.Key},{node.Value.Longitude},{node.Value.Latitude},{node.Value.X},{node.Value.Y}");
+                    //adjacencyMatrix.WriteLine($"{node.Key} {WriteAdjust(node, nodesGeo.Keys)}");
+                    adjacencyList.WriteLine($"{node.Key}:,[{string.Join(" ", node.Value.Adjency)}]");
+                    if (count % 1000 == 0)
+                    {
+                        Console.WriteLine($"Done {count} nodes of {nodesGeo.Count}. It's {count * 100.0 / nodesGeo.Count}%");
+                    }
+                }
+                Console.WriteLine($"There are {edges} of {nodesGeo.Count * nodesGeo.Count} edges. It's {edges * 100.0 / nodesGeo.Count / nodesGeo.Count}% of possible");
             }
 
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Job done! Now it's time for tea");
         }
     }
 }
---
+++ src/Types/GeoPoint.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CityMap.Types
+{
+    public class GeoPoint
+    {
+        private static double ConvertToRadians(double angle)
+        {
+            return Math.PI * angle / 180.0;
+        }
+
+        private void ConvertToMercatorProjection()
+        {
+            var rLat = ConvertToRadians(Latitude);
+            var rLong = ConvertToRadians(Longitude);
+
+            var a = 6378137.0;
+            var b = 6356752.3142;
+            var f = (a - b) / a;
+
+            var e = Math.Sqrt(2 * f - f * f);
+
+            X = a * rLong;
+            Y = a * Math.Log(Math.Tan(Math.PI / 4.0 + rLat / 2.0) * Math.Pow((1 - e * Math.Sin(rLat)) / (1 + e * Math.Sin(rLat)), e / 2.0));
+        }
+
+        public GeoPoint(double longitude, double latitude)
+        {
+            Longitude = longitude;
+            Latitude = latitude;
+            ConvertToMercatorProjection();
+        }
+
+        public double Longitude { get; set; }
+        public double Latitude { get; set; }
+        public bool Used { get; set; }
+
+        public double X { get; set; }
+        public double Y { get; set; }
+
+        public List<ulong> Adjency { get; } = new List<ulong>();
+    }
+}
