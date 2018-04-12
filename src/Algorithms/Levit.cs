f366b68e9e5a584e2ecc4994dccfc26230a827bb
Yury Belousov <yury-belousov@yandex.ru>
2018-04-12 21:25:40 +0300

    stay information only for the first 1000 points

---
+++ src/Algorithms/Levit.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace CityMap.Algorithms
+{
+    public static class Levit
+    {
+        public static (Dictionary<ulong, double>, Dictionary<ulong, ulong>) Calculate(ulong startId = 4198407189)
+        {
+
+        }
+    }
+}
