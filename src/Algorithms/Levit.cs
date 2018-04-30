f9b00e9af9c1bdf213c83d7aea5321deba0e2328
Yury Belousov <yury-belousov@yandex.ru>
2018-04-30 19:46:57 +0300

    renaming

--- src/Algorithms/Levit.cs
+++ src/Algorithms/Levit.cs
@@ -19,44 +19,43 @@ namespace CityMap.Algorithms
             var distance = SvgHelper.Dictionary.ToDictionary(k => k.Key, v => double.MaxValue);
             var p = new Dictionary<ulong, ulong>(SvgHelper.Dictionary.Count);
             var status = SvgHelper.Dictionary.ToDictionary(k => k.Key, v => Status.NotYet);
-            var queue = new Queue<ulong>(SvgHelper.Dictionary.Count);
-            var mainQueue = new Queue<ulong>(SvgHelper.Dictionary.Count);
+            var queue = new Queue<ulong>();
+            var priorityQueue = new Queue<ulong>();
 
             distance[startId] = 0;
             status[startId] = Status.Now;
             queue.Enqueue(startId);
 
-            while (queue.Any() || mainQueue.Any())
+            while (queue.Any() || priorityQueue.Any())
             {
-                var u = mainQueue.Any() ? mainQueue.Dequeue() : queue.Dequeue();
+                var u = priorityQueue.Any() ? priorityQueue.Dequeue() : queue.Dequeue();
 
-                foreach (var v in SvgHelper.Dictionary[u].Adjency)
+                foreach (var to in SvgHelper.Dictionary[u].Adjency)
                 {
-                    var len = distance[u] + SvgHelper.Dictionary[u].DistanceBetweenPoints(SvgHelper.Dictionary[v]);
-                    switch (status[v])
+                    var len = distance[u] + SvgHelper.Dictionary[u].DistanceBetweenPoints(SvgHelper.Dictionary[to]);
+                    switch (status[to])
                     {
                         case Status.NotYet:
-                            queue.Enqueue(v);
-                            status[v] = Status.Now;
-                            if (distance[v] > len)
+                            queue.Enqueue(to);
+                            status[to] = Status.Now;
+                            if (distance[to] > len)
                             {
-                                distance[v] = len;
-                                p[v] = u;
+                                distance[to] = len;
+                                p[to] = u;
                             }
                             break;
                         case Status.Now:
-                            if (distance[v] > len)
+                            if (distance[to] > len)
                             {
-                                distance[v] = len;
-                                p[v] = u;
+                                distance[to] = len;
+                                p[to] = u;
                             }
                             break;
-                        case Status.Already when distance[v] >
-                             len:
-                            mainQueue.Enqueue(v);
-                            status[v] = Status.Now;
-                            distance[v] = len;
-                            p[v] = u;
+                        case Status.Already when distance[to] > len:
+                            priorityQueue.Enqueue(to);
+                            status[to] = Status.Now;
+                            distance[to] = len;
+                            p[to] = u;
                             break;
                     }
                 }
